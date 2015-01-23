b17a178574f80bd794238783e57ae66fb5301fc1
tristen <tristen.brown@gmail.com>
2015-01-22 23:36:27 -0500

    Build via npm. Lil project tidying

--- ender.js
+++ ender.js
@@ -1,9 +1,9 @@
-!function ($) {
-    $.ender({
-        hoverintent: function (over, out) {
-            return this.forEach(function(el) {
-                hoverintent(el, over, out).options(opt || {});
-            });
-        }
-    }, true);
+!function($) {
+  $.ender({
+    hoverintent: function(over, out) {
+      return this.forEach(function(el) {
+        hoverintent(el, over, out).options(opt || {});
+      });
+    }
+  }, true);
 }(ender);
